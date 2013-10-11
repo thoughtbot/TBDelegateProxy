7aa9fdaa279cb13db415e5b46ebb937c9504e6d9
Mark Adams <mark@thoughtbot.com>
2013-10-11 14:09:25 -0700

    Initial Commit

---
+++ TBDelegateProxy/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  TBDelegateProxy
+//
+//  Created by Mark Adams on 10/11/13.
+//  Copyright (c) 2013 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TBDelegateProxy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TBDelegateProxy
+//
+//  Created by Mark Adams on 10/11/13.
+//  Copyright (c) 2013 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBAppDelegate class]));
+    }
+}
